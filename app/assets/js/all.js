8f08a280ec89e065b72a565a110fcf2796710287
Vic <Vic@gmail.com>
2021-08-23 17:44:03 +0800

    [ add ] 師資區塊加入 swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,24 @@
 $(function () {
   
 });
+
+var swiper = new Swiper(".teachersSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  breakpoints: {
+    576: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+  
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+  
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
